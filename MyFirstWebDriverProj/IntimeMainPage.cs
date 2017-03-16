d84fadc8a44bf8166f49f9b888fe19987884d8ef
gonardfreeman <007arenko@gmail.com>
2017-03-16 18:06:39 +0200

    first commit

---
+++ MyFirstWebDriverProj/IntimeMainPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace MyFirstWebDriverProj
+{
+    class IntimeMainPage
+    {
+        private IWebDriver driver;
+
+        public IntimeMainPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Navigate(string url)
+        {
+            this.driver.Url = url;
+        }
+    }
+}
---
+++ MyFirstWebDriverProj/IntimeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+
+namespace MyFirstWebDriverProj
+{
+    [TestClass]
+    public class IntimeTests
+    {
+        public IWebDriver Driver { get; set; }
+        
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new FirefoxDriver();
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SearchTestField()
+        {
+            IntimeMainPage intimeMainPage = new IntimeMainPage(this.Driver);
+            intimeMainPage.Navigate("http://intime.ua");
+        }
+    }
+}
